9de1f005b5c21ea4da68b874d7090228ffd62b5c
akitotheanimator <sapressaonenem@gmail.com>
2025-04-20 05:28:23 -0300

    Add files via upload

---
+++ source/ioprp310/bkp/Timer_Manager.C
@@ -0,0 +1,18 @@
+void start()
+{
+   var var_t0 = 0
+   unsigned int v0;
+
+   goto load_function_0;
+
+
+   
+   load_function_0:
+   //var v0 holds the Processor Revision Identifier, Moving the Processor ID register to v0.
+   //nop
+      if (v0 < 16) {
+          goto loc_70;
+      } else {
+          v0 = 1;
+      }
+}
